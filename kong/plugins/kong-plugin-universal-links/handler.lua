3e0004e1527bda2403d5937a5dd3effcd8105828
Chettha Thawornsathit <roticagas@crosstypedev.com>
2019-12-29 20:19:37 +0700

    add print

--- kong/plugins/kong-plugin-universal-links/handler.lua
+++ kong/plugins/kong-plugin-universal-links/handler.lua
@@ -17,22 +17,26 @@ function plugin:access(conf)
   -- kong.* functions are from the PDK (plugin development kit)
   -- and do not need to be explicitly required
   self.agent = kong.request.get_header(conf.headerName)
+  print("agent " + self.agent)
 end
 
 function plugin:header_filter(conf)
   plugin.super.header_filter(self)
-
+  
   if string.find(self.agent, conf.androidKeyword) then
+    print("found " + conf.androidKeyword)
     if conf.androidLink ~= "" then
       return kong.response.exit(302, conf.androidLink)
     end
   end
   if string.find(self.agent, conf.iphoneKeyword) then
+    print("found " + conf.iphoneKeyword)
     if conf.iphoneLink ~= "" then
       return kong.response.exit(302, conf.iphoneLink)
     end
   end
   if string.find(self.agent, conf.ipadKeyword) then
+    print("found " + conf.ipadKeyword)
     if conf.ipadLink ~= "" then
       return kong.response.exit(302, conf.ipadLink)
     end
