d00fb60a42e38d74a746418decfd8f85b2899829
Chettha Thawornsathit <roticagas@crosstypedev.com>
2019-12-29 20:48:45 +0700

    fix bug and print all

--- kong/plugins/kong-plugin-universal-links/handler.lua
+++ kong/plugins/kong-plugin-universal-links/handler.lua
@@ -12,19 +12,26 @@ end
 -- Run this when the client request hits the service
 function plugin:access(conf)
   plugin.super.access(self)
-
+  print(conf.headerName)
+  print(conf.androidKeyword)
+  print(conf.androidLink)
+  print(conf.iphoneKeyword)
+  print(conf.iphoneLink)
+  print(conf.ipadKeyword)
+  print(conf.ipadLink)
   -- kong.* functions are from the PDK (plugin development kit)
   -- and do not need to be explicitly required
   local agent = kong.request.get_header(conf.headerName)
-  if string.find(self.agent, conf.androidKeyword) then
+  print(agent)
+  if string.find(agent, conf.androidKeyword) then
     return kong.response.exit(302, conf.androidLink)
   end
 
-  if string.find(self.agent, conf.iphoneKeyword) then
+  if string.find(agent, conf.iphoneKeyword) then
     return kong.response.exit(302, conf.iphoneLink)
   end
 
-  if string.find(self.agent, conf.ipadKeyword) then
+  if string.find(agent, conf.ipadKeyword) then
     return kong.response.exit(302, conf.ipadLink)
   end
 end
